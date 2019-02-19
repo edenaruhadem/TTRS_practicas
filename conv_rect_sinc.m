ed26627a45e2bff193ed1431fd288589d51fcedd
EdenaRuh <diego.rios.ctag@gmail.com>
2019-02-19 09:06:46 +0100

    update

--- conv_rect_sinc.m
+++ conv_rect_sinc.m
@@ -1,12 +1,15 @@
 T = 1;
 ampli_pulse = T;
+ampli_tripulse = T;
 fs = 4000;
 t = -5:1/fs:5;
 tt = -10:1/fs:10;
 x = sinc(t/T);
 y = rectpuls(t,ampli_pulse);
+z = tripuls(t,ampli_tripulse);
 convolucion = conv(x,y);
 %plot (t,x)
 %plot(t,y)
-plot(tt,convolucion)
+plot(t,z)
+%plot(tt,convolucion)
 grid on
